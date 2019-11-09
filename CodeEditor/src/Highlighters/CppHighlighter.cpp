279e0507efb6b15c74011e7b7d2652fcf28d4f22
Clément Plantec <plantec.clement@gmail.com>
2019-11-09 17:03:09 +0100

    update highlighter

--- CodeEditor/src/Highlighters/CppHighlighter.cpp
+++ CodeEditor/src/Highlighters/CppHighlighter.cpp
@@ -1,5 +1,19 @@
 #include "CppHighlighter.h"
 
+//TODO: for enum, since it start with a Capital letter, it's treated like a class
+// so check case like Qt::Test and QTextCursor::MoveOperation::Left (only Test and Left should be yellow)
+
+QVector<BracketsInfo*> TextBlockData::brackets()
+{
+	return m_brackets;
+}
+
+
+void TextBlockData::append(BracketsInfo* info)
+{
+	m_brackets.append(info);
+}
+
 
 CppHighlighter::CppHighlighter(QTextDocument* parent)
 	:BaseHighlighter(parent)
@@ -14,21 +28,18 @@ CppHighlighter::CppHighlighter(QTextDocument* parent)
 
 	HighlightingRule rule;
 
-	keywordFormat.setForeground(Qt::darkBlue);
-	//keywordFormat.setFontWeight(QFont::Bold);
-	const QString keywordPatterns[] = 
+	keywordFormat.setForeground(QColor(70, 156, 208));
+	const QString keywordPatterns[] =
 	{
-		QStringLiteral("\\bchar\\b"), QStringLiteral("\\bclass\\b"), QStringLiteral("\\bconst\\b"),
-		QStringLiteral("\\bdouble\\b"), QStringLiteral("\\benum\\b"), QStringLiteral("\\bexplicit\\b"),
-		QStringLiteral("\\bfriend\\b"), QStringLiteral("\\binline\\b"), QStringLiteral("\\bint\\b"),
-		QStringLiteral("\\blong\\b"), QStringLiteral("\\bnamespace\\b"), QStringLiteral("\\boperator\\b"),
-		QStringLiteral("\\bprivate\\b"), QStringLiteral("\\bprotected\\b"), QStringLiteral("\\bpublic\\b"),
-		QStringLiteral("\\bshort\\b"), QStringLiteral("\\bsignals\\b"), QStringLiteral("\\bsigned\\b"),
-		QStringLiteral("\\bslots\\b"), QStringLiteral("\\bstatic\\b"), QStringLiteral("\\bstruct\\b"),
-		QStringLiteral("\\btemplate\\b"), QStringLiteral("\\btypedef\\b"), QStringLiteral("\\btypename\\b"),
-		QStringLiteral("\\bunion\\b"), QStringLiteral("\\bunsigned\\b"), QStringLiteral("\\bvirtual\\b"),
-		QStringLiteral("\\bvoid\\b"), QStringLiteral("\\bvolatile\\b"), QStringLiteral("\\bbool\\b"),
-		QStringLiteral("\\bthis\\b")
+		R"(\basm\b)", R"(\bauto\b)", R"(\bbool\b)", R"(\bcatch\b)", R"(\bchar\b)", R"(\bclass\b)", R"(\bconst\b)",
+		R"(\bconst_cast\b)", R"(\bdelete\b)", R"(\bdynamic_cast\b)", R"(\bdouble\b)", R"(\benum\b)", R"(\bexplicit\b)",
+		R"(\bexport\b)", R"(\bextern\b)", R"(\bfalse\b)", R"(\bfloat\b)", R"(\bfriend\b)", R"(\binline\b)", R"(\bint\b)",
+		R"(\blong\b)", R"(\bmutable\b)", R"(\bnamespace\b)", R"(\bnew\b)", R"(\bnullptr\b)", R"(\boperator\b)",
+		R"(\bprivate\b)", R"(\bprotected\b)", R"(\bpublic\b)", R"(\bregister\b)", R"(\breinterpret_cast\b)", R"(\bshort\b)",
+		R"(\bsignals\b)", R"(\bsigned\b)", R"(\bsizeof\b)", R"(\bslots\b)", R"(\bstatic\b)", R"(\bstatic_cast\b)",
+		R"(\bstruct\b)", R"(\btemplate\b)", R"(\bthis\b)", R"(\bthrow\b)", R"(\btrue\b)", R"(\btry\b)", R"(\btypedef\b)",
+		R"(\btypeid\b)", R"(\btypename\b)", R"(\bunion\b)", R"(\bunsigned\b)", R"(\busing\b)", R"(\bvirtual\b)", 
+		R"(\bvoid\b)", R"(\bvolatile\b)", R"(\bwchar_t\b)",
 	};
 
 	for (const QString& pattern : keywordPatterns) 
@@ -38,32 +49,103 @@ CppHighlighter::CppHighlighter(QTextDocument* parent)
 		highlightingRules.append(rule);
 	}
 
-	/*classFormat.setFontWeight(QFont::Bold);*/
-	classFormat.setForeground(Qt::blue);
-	rule.pattern = QRegularExpression(QStringLiteral("\\b[A-Z][A-Za-z]+\\b"));
+	controlFlowFormat.setForeground(QColor(216, 160, 223));
+	const QString controlFlowPatterns[] =
+	{
+		R"(\bif\b)", R"(\belse\b)", R"(\bdo\b)", R"(\bwhile\b)", R"(\breturn\b)", R"(\bbreak\b)", 
+		R"(\bcontinue\b)", R"(\bfor\b)", R"(\bswitch\b)", R"(\bcase\b)", R"(\bdefault\b)", R"(\bgoto\b)"
+	};
+
+	for (const QString& pattern : controlFlowPatterns)
+	{
+		rule.pattern = QRegularExpression(pattern);
+		rule.format = controlFlowFormat;
+		highlightingRules.append(rule);
+	}
+
+	classFormat.setForeground(QColor(78, 201, 176));
+	rule.pattern = QRegularExpression(QStringLiteral("\\b[A-Z][A-Za-z]+(?=\\()?\\b"));
 	rule.format = classFormat;
 	highlightingRules.append(rule);
 
-	quotationFormat.setForeground(Qt::darkGreen);
-	rule.pattern = QRegularExpression(QStringLiteral("\".*\""));
-	rule.format = quotationFormat;
-	highlightingRules.append(rule);
-
-	functionFormat.setFontItalic(true);
-	functionFormat.setForeground(Qt::darkYellow);
+	functionFormat.setForeground(QColor(220, 220, 170));
 	rule.pattern = QRegularExpression(QStringLiteral("\\b[A-Za-z0-9_]+(?=\\()"));
 	rule.format = functionFormat;
 	highlightingRules.append(rule);
 
-	singleLineCommentFormat.setForeground(Qt::red);
+	classFunctionFormat.setForeground(QColor(220, 220, 170));
+	rule.pattern = QRegularExpression(R"((?<=::)\w+)");
+	rule.format = classFunctionFormat;
+	highlightingRules.append(rule);
+	
+	preProcessorFormat.setForeground(Qt::darkGray);
+	rule.pattern = QRegularExpression(QStringLiteral("#.*"));
+	rule.format = preProcessorFormat;
+	highlightingRules.append(rule);
+	
+	includeFormat.setForeground(QColor(214, 157, 133));
+	/*rule.pattern = QRegularExpression(R"((?=)<[a-zA-Z0-9*._]+>)");*/
+	rule.pattern = QRegularExpression(R"((?<=#include)(?:\s*)<[a-zA-Z0-9*._]+>)");
+	rule.format = includeFormat;
+	highlightingRules.append(rule);
+
+	numbersFormat.setForeground(QColor(156, 220, 254));
+	rule.pattern = QRegularExpression(R"(-?\b(0b|0x){0,1}[\d.']+\b)");
+	rule.format = numbersFormat;
+	highlightingRules.append(rule);
+	
+	quotationFormat.setForeground(QColor(214, 157, 133));
+	rule.pattern = QRegularExpression(QStringLiteral("\".*\""));
+	rule.format = quotationFormat;
+	highlightingRules.append(rule);
+	
+	singleLineCommentFormat.setForeground(QColor(78, 163, 58));
 	rule.pattern = QRegularExpression(QStringLiteral("//[^\n]*"));
 	rule.format = singleLineCommentFormat;
 	highlightingRules.append(rule);
-
-	multiLineCommentFormat.setForeground(Qt::red);
-
-	commentStartExpression = QRegularExpression(QStringLiteral("/\\*"));
-	commentEndExpression = QRegularExpression(QStringLiteral("\\*/"));
+	
+	multiLineCommentFormat.setForeground(QColor(78, 163, 58));
+	
+	commentStartPattern = QRegularExpression(R"(/\*)");
+	commentEndPattern = QRegularExpression(R"(\*/)");
+	
+	//includeFormat.setForeground(QColor(210, 134, 84));
+	//rule.pattern = QRegularExpression(R"(#include\s+([<"][a-zA-Z0-9*._]+[">]))");
+	//rule.format = includeFormat;
+	//highlightingRules.append(rule);
+	//
+	//functionFormat.setForeground(QColor(219, 203, 121));
+	//rule.pattern = QRegularExpression(R"(\b([A-Za-z0-9_]+(?:\s+|::))*([A-Za-z0-9_]+)(?=\())");
+	//rule.format = functionFormat;
+	//highlightingRules.append(rule);
+	//
+	//defTypeFormat.setForeground(QColor(78, 201, 176));
+	//rule.pattern = QRegularExpression(R"(\b([A-Za-z0-9_]+)\s+[A-Za-z]{1}[A-Za-z0-9_]+\s*[;=])");
+	//rule.format = defTypeFormat;
+	//highlightingRules.append(rule);
+	//
+	//preProcessorFormat.setForeground(Qt::darkGray);
+	//rule.pattern = QRegularExpression(R"(#[a-zA-Z_]+)");
+	//rule.format = preProcessorFormat;
+	//highlightingRules.append(rule);
+	//
+	//numbersFormat.setForeground(QColor(156, 220, 254));
+	//rule.pattern = QRegularExpression(R"(\b(0b|0x){0,1}[\d.']+\b)");
+	//rule.format = numbersFormat;
+	//highlightingRules.append(rule);
+	//
+	//stringFormat.setForeground(QColor(210, 134, 84));
+	//rule.pattern = QRegularExpression(R"("[^\n"]*")");
+	//rule.format = stringFormat;
+	//highlightingRules.append(rule);
+	//
+	//commentFormat.setForeground(QColor(78, 163, 58));
+	//rule.pattern = QRegularExpression(R"(//[^\n]*)");
+	//rule.format = commentFormat;
+	//highlightingRules.append(rule);
+	//
+	//commentStartPattern = QRegularExpression(R"(/\*)");
+	//commentEndPattern = QRegularExpression(R"(\*/)");
 }
 
 
@@ -90,25 +172,48 @@ void CppHighlighter::highlightBlock(const QString& text)
 
 	int startIndex = 0;
 	if (previousBlockState() != 1)
-		startIndex = text.indexOf(commentStartExpression);
+		startIndex = text.indexOf(commentStartPattern);
 
 	while (startIndex >= 0) 
 	{
-		QRegularExpressionMatch match = commentEndExpression.match(text, startIndex);
+		QRegularExpressionMatch match = commentEndPattern.match(text, startIndex);
 		int endIndex = match.capturedStart();
 		int commentLength = 0;
-		if (endIndex == -1) {
+		if (endIndex == -1) 
+		{
 			setCurrentBlockState(1);
 			commentLength = text.length() - startIndex;
 		}
-		else {
+		else 
+		{
 			commentLength = endIndex - startIndex
 				+ match.capturedLength();
 		}
 		setFormat(startIndex, commentLength, multiLineCommentFormat);
-		startIndex = text.indexOf(commentStartExpression, startIndex + commentLength);
+		startIndex = text.indexOf(commentStartPattern, startIndex + commentLength);
 	}
 
+
+	// for each textblock, store its indentation level
+	TextBlockData* data = new TextBlockData;
+
+
+
+
+
+	/*int indentCounter = 0;
+	while (text.size() > indentCounter)
+	{
+		if (text.at(indentCounter).toLatin1() != '\t')
+			break;
+
+		++indentCounter;
+	}
+
+	if (indentCounter > 1)
+		data->indentLevel = indentCounter;*/
+
+	setCurrentBlockUserData(data);
 }
 
 
--- CodeEditor/src/Highlighters/CppHighlighter.h
+++ CodeEditor/src/Highlighters/CppHighlighter.h
@@ -7,6 +7,26 @@
 #include <QTextDocument>
 
 
+struct BracketsInfo
+{
+	int startPosition;
+	int endPosition;
+};
+
+class TextBlockData : 
+	public QTextBlockUserData
+{
+public:
+	QVector<BracketsInfo*> brackets();
+	void append(BracketsInfo* info);
+
+private:
+	QVector<BracketsInfo*> m_brackets;
+
+};
+
+
+
 class CppHighlighter :
 	public BaseHighlighter
 {
@@ -27,19 +47,37 @@ private:
 		QTextCharFormat format;
 	};
 
+	// defines the braces for c++
 	const QVector<QPair<QChar, QChar>>* m_braces;
+	QVector<int> startBrackets();
+	//
 	
 
 	QVector<HighlightingRule> highlightingRules;
 
-	QRegularExpression commentStartExpression;
-	QRegularExpression commentEndExpression;
+	QRegularExpression commentStartPattern;
+	QRegularExpression commentEndPattern;
 
 	QTextCharFormat keywordFormat;
+	QTextCharFormat controlFlowFormat;
+	QTextCharFormat functionFormat;
+	QTextCharFormat classFunctionFormat;
 	QTextCharFormat classFormat;
+	QTextCharFormat preProcessorFormat;
+	QTextCharFormat includeFormat;
 	QTextCharFormat singleLineCommentFormat;
 	QTextCharFormat multiLineCommentFormat;
 	QTextCharFormat quotationFormat;
+	QTextCharFormat numbersFormat;
+	
+
+	/*QTextCharFormat keywordFormat;
+	QTextCharFormat includeFormat;
 	QTextCharFormat functionFormat;
+	QTextCharFormat defTypeFormat;
+	QTextCharFormat preProcessorFormat;
+	QTextCharFormat numbersFormat;
+	QTextCharFormat stringFormat;
+	QTextCharFormat commentFormat;*/
 };
 
