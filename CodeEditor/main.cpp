32e73f7ba7f826f06aaf8f23466c62949dbe4447
Clément Plantec <plantec.clement@gmail.com>
2019-11-09 17:02:53 +0100

    update project

--- CodeEditor/main.cpp
+++ CodeEditor/main.cpp
@@ -4,6 +4,13 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+	QFile File("Resources/StyleSheetDark.qss");
+	File.open(QFile::ReadOnly);
+	QString StyleSheet = QLatin1String(File.readAll());
+	qApp->setStyleSheet(StyleSheet);
+
+
 	CodeEditor w;
 	w.show();
 	return a.exec();
--- CodeEditor/src/CodeEditor.cpp
+++ CodeEditor/src/CodeEditor.cpp
@@ -11,12 +11,13 @@ CodeEditor::CodeEditor(QWidget *parent)
 	: QMainWindow(parent), ui(new Ui::CodeEditorClass)
 {
 	ui->setupUi(this);
-
+	/*ui->mainToolBar->setIconSize(QSize(16, 16));*/
+	ui->mainToolBar->setFixedHeight(30);
 
 	QFont font;
 	font.setFamily("Consolas");
 	font.setFixedPitch(true);
-	font.setPointSize(10);
+	font.setPointSize(11);
 
 	ui->editor->setFont(font);
 
@@ -25,6 +26,7 @@ CodeEditor::CodeEditor(QWidget *parent)
 
 
 	QFile file("src/Widgets/Editor.cpp");
+	//QFile file("test.txt");
 	if (file.open(QFile::ReadOnly | QFile::Text))
 		ui->editor->setPlainText(file.readAll());
 }
